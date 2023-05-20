40b6014406a98dc8eef3b34cb2401204b2e3bf5b
Guilherme Carvalho <guilhermewallis@protonmail.com>
2023-05-20 16:14:05 +0100

    first

---
+++ Lab_intro.sql
@@ -0,0 +1,21 @@
+-- 1. Use sakila database
+use sakila;
+
+
+-- 2. Get all the data from tables actor, film and customer
+select * from actor, film, customer;
+
+-- 3. Get film titles
+select title from film;
+
+-- 4. Get unique list of film languages under the alias language
+select name from language;
+
+-- 5.1. Find out how many stores does the company have
+select count(*) from store;
+
+-- 5.2. Find out how many employees staff does the company have
+select count(*) from staff;
+
+-- 5.3. Return a list of employee first names only
+select first_name from staff;
