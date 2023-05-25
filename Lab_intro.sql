38a382565081e4a0efe345bf90f27865a047e7d6
Guilherme Carvalho <guilhermewallis@protonmail.com>
2023-05-25 23:55:12 +0100

    correction

--- Lab_intro.sql
+++ Lab_intro.sql
@@ -3,13 +3,15 @@ use sakila;
 
 
 -- 2. Get all the data from tables actor, film and customer
-select * from actor, film, customer;
+select * from actor;
+select * from film;
+select * from customer;
 
 -- 3. Get film titles
 select title from film;
 
 -- 4. Get unique list of film languages under the alias language
-select name from language;
+select name as language from language;
 
 -- 5.1. Find out how many stores does the company have
 select count(*) from store;
